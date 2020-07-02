9150a2d67b568f59b27f56bc1f36ffa91e19ba7a
Paulina Bulatecka <pabulatecka@gmail.com>
2020-07-02 23:09:12 +0200

    initial commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-console.log('STRONA DO EDUKACJI');
-const firstName = 'Paulina';
+console.log("STRONA DO EDUKACJI");
+const firstName = "Paulina";
 const age = 23;
 
 console.log(firstName);
 console.log(age);
-console.log(`Siema nazywam się ${firstName} i mam ${age} lata`)
+console.log(`Siema nazywam się ${firstName} i mam ${age} lata`);
