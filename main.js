189e66152f68ef781671e5cc0d466806fa3775c4
Paulina Bulatecka <pabulatecka@gmail.com>
2020-07-06 14:35:09 +0200

    homework with JS

--- main.js
+++ main.js
@@ -4,4 +4,9 @@ const age = 23;
 
 console.log(firstName);
 console.log(age);
-console.log(`Siema nazywam się ${firstName} i mam ${age} lata`);
+console.log(`Cześć nazywam się ${firstName} i mam ${age} lata`);
+
+ const heading = document.querySelector('.headerFirst--js');
+ 
+ heading.innerHTML=`Cześć nazywam się ${firstName} i mam ${age} lata`;
+
