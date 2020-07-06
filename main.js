a64251b6453880fc341a309b1a9ce550d44f5a8e
Paulina Bulatecka <pabulatecka@gmail.com>
2020-07-06 16:05:34 +0200

    news classes

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ console.log(firstName);
 console.log(age);
 console.log(`Cześć nazywam się ${firstName} i mam ${age} lata`);
 
- const heading = document.querySelector('.headerFirst--js');
+ const heading = document.querySelector('.main__heading--js');
  
  heading.innerHTML=`Cześć nazywam się ${firstName} i mam ${age} lata`;
 
